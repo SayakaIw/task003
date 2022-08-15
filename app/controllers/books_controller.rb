2a4fba32f7eb6e7b71174566a31e63601af55aa0
SayakaIw <160105syk@gmail.com>
2022-08-15 14:02:41 +0000

    index_end

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,21 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(books_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
   end
 
   def index
+    @user = current_user
   end
 
   def show
+
   end
 
   def edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,21 @@
 class UsersController < ApplicationController
-  
+
   def show
-    
+    @user = User.find(params[:id])
+    @book = @user.books
   end
 
   def edit
-    
+    @user = User.find(params[:id])
   end
-  
+
   def index
-    
+    @user = User.find(params[:id])
   end
-  
+
   def update
-    
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 end
